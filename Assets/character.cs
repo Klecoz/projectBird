ecccf2f7c30dc18778f08954d07a9bc95c936226
Arsenio Colon <arseniocolon@gmail.com>
2017-11-18 16:27:10 -0500

    We added in sprites and pretty much every other feature maybe.

--- Assets/character.cs
+++ Assets/character.cs
@@ -1,20 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class character : MonoBehaviour {
 
 	public bool isJump = false;
+	private Vector2 startingPosition;
+
 	// Use this for initialization
 	void Start () {
-		
+		startingPosition = gameObject.transform.position;
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
- var speed = 3.0f;  // ...or whatever.
+ var speed = 12.0f;  // ...or whatever.
 
 
 
@@ -28,17 +31,11 @@ if (Input.GetKey("a")) {
 
 if (Input.GetKeyDown("w") && isJump == false) {
    GetComponent<Rigidbody2D>().AddForce(new Vector2(0,16), ForceMode2D.Impulse);
-   isJump = true;
-   StartCoroutine(timeJump());
+	isJump = true;
 }
 
 	}
 
-	IEnumerator timeJump()
-    {
-        yield return new WaitForSeconds(1);
-        isJump = false;
-    }
 
 void OnCollisionEnter2D(Collision2D coll)
     {
@@ -48,5 +45,40 @@ void OnCollisionEnter2D(Collision2D coll)
                      Debug.Log("I am touching coin");
 					 Destroy(coll.gameObject);
                 }
+
+			if (coll.gameObject.CompareTag("platform"))
+                {
+                     Debug.Log("I am touching the platform");
+					  isJump = false; 
+                }
+
+
+			if (coll.gameObject.CompareTag("beehive"))
+                {
+                     Debug.Log("I am touching the beehive");
+					 gameObject.transform.position = startingPosition;
+                }
+
+			if (coll.gameObject.CompareTag("deathLine"))
+                {
+                     Debug.Log("I am touching the kill Line");
+					 SceneManager.LoadScene("projectBird");
+                }
+
+				if (coll.gameObject.CompareTag("leaf"))
+                {
+                     Debug.Log("I am touching the leaf");
+					 isJump = true; 
+					 GetComponent<Rigidbody2D>().AddForce(new Vector2(0,26), ForceMode2D.Impulse);
+                }
+
+				if (coll.gameObject.CompareTag("breakbranch"))
+                {
+                     Debug.Log("I am touching the break branch");
+					 isJump = false; 
+					 Destroy(coll.gameObject, .6f);
+					 
+                }
+			
     }
 }
